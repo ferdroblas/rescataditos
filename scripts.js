c2e1035bd44a4b39c3c7e2c9d04d5a2ba41235f0
salbord <fernandodroblas@gmail.com>
2025-01-10 22:00:04 -0300

    mejoria del diseno

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  menuToggle.addEventListener("click", () => {
+    mobileMenu.classList.toggle("menu-open");
+  });
+});
